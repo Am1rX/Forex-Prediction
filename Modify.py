1e1ba7f87295d69ac41035f2687812e78780f4eb
AmirMahdi <private.amirx@gmail.com>
2023-11-22 12:14:50 +0330

    Add files via upload

---
+++ Modify.py
@@ -0,0 +1,9 @@
+import pandas as pd
+file_name = input("Enter Yout CSV file to be prepared for prediction [it should be from metatrader history data ! ] >> ")
+df = pd.read_csv(file_name, header=None)
+df[0] = df[0].astype(str) + " " + df[1].astype(str)
+df = df.drop(columns=[6])
+df = df.drop(columns=[1])
+df[0] = pd.to_datetime(df[0], format='%Y.%m.%d %H:%M')
+df[0] = df[0].dt.strftime('%m/%d/%Y %I:%M:%S %p')
+df.to_csv("nozari.csv", index=False, header=None)
